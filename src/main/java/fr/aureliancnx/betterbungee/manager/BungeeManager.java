932794603a6afb39b5c50371782e53ea779db4a6
aureliancnx <aurelien.marcel@epitech.eu>
2022-05-30 12:41:12 +0200

    ADD: bungeecord manager base

---
+++ src/main/java/fr/aureliancnx/betterbungee/manager/BungeeManager.java
@@ -0,0 +1,92 @@
+package fr.aureliancnx.betterbungee.manager;
+
+import fr.aureliancnx.betterbungee.BetterBungeePlugin;
+import fr.aureliancnx.betterbungee.api.proxy.IBungeeServer;
+import fr.aureliancnx.betterbungee.impl.proxy.MyProxy;
+import fr.aureliancnx.betterbungee.packet.bungee.PacketProxyPing;
+import net.md_5.bungee.api.ChatColor;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class BungeeManager implements IBungeeManager {
+
+    private final MyProxy                               my;
+    private final ConcurrentMap<String, IBungeeServer>  bungeeByNames;
+
+    public BungeeManager(final String localBungeeName, final int localSlotCount) {
+        this.my = new MyProxy(localBungeeName, localSlotCount);
+        this.bungeeByNames = new ConcurrentHashMap<>();
+        this.bungeeByNames.put(proxyName, local);
+    }
+
+    public BungeeManager(final BetterBungeeConfig config) {
+        this(ProxyUtils.lookupProxyName(config), config.getSlots());
+    }
+
+    @Override
+    public IBungeeServer getProxy(final String name) {
+        if (name.equals(this.my.getBungeeName()))
+            return this.my;
+
+        final IBungeeServer bungeeServer = bungeeByNames.get(name);
+        if (bungeeServer == null)
+            return null;
+        return bungeeServer.isAlive() ? bungeeServer : null;
+    }
+
+    @Override
+    public Collection<IBungeeServer> getProxies() {
+        return getProxies(proxyServer -> true);
+    }
+
+    @Override
+    public Iterator<Map.Entry<String, IBungeeServer>> iterator() {
+        return bungeeByNames.entrySet().iterator();
+    }
+
+    @Override
+    public Collection<IBungeeServer> getProxies(Predicate<IBungeeServer> predicate) {
+        return bungeeByNames.values().stream()
+                .filter(IBungeeServer::isAlive)
+                .filter(predicate)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public IBungeeServer add(final PacketProxyKeepAlive keepAlive) {
+        final IBungeeServer server = new BungeeServer(keepAlive.getProxyName(), keepAlive.getSlots(),
+                keepAlive.getPlayers());
+
+        bungeeByNames.put(server.getProxyName(), server);
+        BetterBungeePlugin.getInstance().getLogger().info("[BetterBungee] " + ChatColor.GREEN + "Registered running proxy: " + server.getProxyName());
+        return server;
+    }
+
+    @Override
+    public IBungeeServer remove(final String proxyName) {
+        return bungeeByNames.remove(proxyName);
+    }
+
+    @Override
+    public IBungeeServer update(final PacketProxyPing keepAlive) {
+        final IBungeeServer bungee = getProxy(keepAlive.getProxyName());
+
+        if (bungee == null) {
+            return add(keepAlive);
+        }
+        bungee.update(keepAlive);
+        return bungee;
+    }
+
+    @Override
+    public MyProxy getMy() {
+        return this.my;
+    }
+
+}
---
+++ src/main/java/fr/aureliancnx/betterbungee/manager/IBungeeManager.java
@@ -0,0 +1,75 @@
+package fr.aureliancnx.betterbungee.manager;
+
+import fr.aureliancnx.betterbungee.api.proxy.IBungeeServer;
+import fr.aureliancnx.betterbungee.impl.proxy.MyProxy;
+import fr.aureliancnx.betterbungee.packet.bungee.PacketProxyPing;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.function.Predicate;
+
+public interface IBungeeManager {
+
+    /**
+     * Get bungeecord server by name.
+     * If the bungeecord name is the current running bungeecord,
+     * it returns the exact instance of MyProxy
+     * @param name bungee name (case insensitive)
+     * @return IBungeeServer
+     */
+    IBungeeServer getBungee(final String name);
+
+    /**
+     * Get all bungee server objects
+     * @return a collection of IBungeeServer
+     */
+    Collection<IBungeeServer> getBungees();
+
+    /**
+     * Creates an iterator of entries with, accordingly,
+     * the bungee name and the instance of IBungeeServer.
+     * @return iterator of Map.Entry<String, IBungeeServer>
+     */
+    Iterator<Map.Entry<String, IBungeeServer>> iterator();
+
+    /**
+     * Get all bungeecord objects that matches a
+     * specific predicate, provided in argument.
+     * @param predicate predicate of IBungeeServer
+     * @return a collection of IBungeeServer
+     */
+    Collection<IBungeeServer> getBungees(Predicate<IBungeeServer> predicate);
+
+    /**
+     * Add a bungee in the bungee manager by using
+     * the bungee ping packet
+     * @param bungeePing PacketBungeePing
+     * @return Bungee server instance
+     */
+    IBungeeServer addBungee(final PacketProxyPing bungeePing);
+
+    /**
+     * Removes a bungeecord from the bungee manager
+     * by using the bungee name
+     * @param bungeeName exact bungee name (case insensitive)
+     * @return removed bungee server instance or null if unknown
+     */
+    IBungeeServer remove(final String bungeeName);
+
+    /**
+     * Updates bungeecord information (such as player data)
+     * by using the ping packet
+     * @param pingPacket PacketBungeePing
+     * @return Updated or newly created bungee server
+     */
+    IBungeeServer update(final PacketProxyPing pingPacket);
+
+    /**
+     * Get current bungee server
+     * @return MyBungee
+     */
+    MyProxy getMy();
+
+
+}
