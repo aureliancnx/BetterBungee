6e39aeec43ee816b8d0553656b0d6135f3892699
aureliancnx <72009413+aureliancnx@users.noreply.github.com>
2022-06-01 22:50:20 +0200

    ADD: stop packet listener

--- src/main/java/fr/aureliancnx/betterbungee/packet/bungee/PacketBungeeStop.java
+++ src/main/java/fr/aureliancnx/betterbungee/packet/bungee/PacketBungeeStop.java
@@ -3,13 +3,19 @@ package fr.aureliancnx.betterbungee.packet.bungee;
 import com.google.common.io.ByteArrayDataInput;
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
+import fr.aureliancnx.betterbungee.BetterBungeePlugin;
 import fr.aureliancnx.betterbungee.api.bungee.IBungeeServer;
+import fr.aureliancnx.betterbungee.api.event.bungee.BetterBungeeStoppedEvent;
+import fr.aureliancnx.betterbungee.manager.proxy.IBungeeManager;
+import fr.aureliancnx.betterbungee.packet.ListenPacket;
 import fr.aureliancnx.betterbungee.packet.Packet;
 import fr.aureliancnx.betterbungee.rabbit.packet.RabbitPacketType;
 import lombok.Getter;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.ProxyServer;
 
 @Getter
-public class PacketBungeeStop extends Packet {
+public class PacketBungeeStop extends ListenPacket {
 
     private static final String QUEUE_NAME = "betterbungee.proxy.stop";
 
@@ -36,6 +42,22 @@ public class PacketBungeeStop extends Packet {
     @Override
     public void fromBytes(final ByteArrayDataInput input) {
         this.bungeeName = input.readUTF();
+    }
+
+    @Override
+    public void onReceive() {
+        final BetterBungeePlugin plugin = BetterBungeePlugin.getInstance();
+        final IBungeeManager bungeeManager = plugin.getBungeeManager();
+        final IBungeeServer bungeeServer = bungeeManager.removeBungee(bungeeName);
+
+        if (bungeeServer == null) {
+            // Already remove? if so, we don't do anything
+            return;
+        }
+        plugin.getLogger().info("Proxy " + bungeeName + " stopped gracefully.");
+        // Trigger event
+        final BetterBungeeStoppedEvent event = new BetterBungeeStoppedEvent(bungeeServer);
+        ProxyServer.getInstance().getPluginManager().callEvent(event);
     }
 
     @Override
